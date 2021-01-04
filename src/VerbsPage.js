b41310ce3c5367f7f167790b0b193c19bbd1683e
Mathias <mathiaswi177@gmail.com>
2021-01-03 21:49:19 -0800

    Async is driving me insane

--- src/VerbsPage.js
+++ src/VerbsPage.js
@@ -2,14 +2,9 @@ import React, { useEffect} from 'react';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Grid from '@material-ui/core/Grid';
 import { makeStyles } from '@material-ui/core/styles';
-import FormControl from '@material-ui/core/FormControl';
-import FormHelperText from '@material-ui/core/FormHelperText';
-import InputLabel from '@material-ui/core/InputLabel';
-import OutlinedInput from '@material-ui/core/OutlinedInput';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import HomeIcon from '@material-ui/icons/Home';
-import CheckCircleOutlineIcon from '@material-ui/icons/CheckCircleOutline';
 
 import Image from './backgrounds/rheine.png';
 
@@ -45,72 +40,45 @@ const useStyles = makeStyles((theme) => ({
 function ComposedTextField() {
 
     const classes = useStyles();
+    const [data, setData] = React.useState([]);
+    const [displayVerb, setDisplayVerb] = React.useState('');
 
-    const getData = () => {
-
-        fetch('verbs.json', {
-            headers: {
-                'Content-Type': 'application/json',
-                'Accept': 'application/json' 
-            }
-        }).then(function(response){
-            console.log(response)
-            setData(response.json());
-            return response.json();
-        }).then(function(myJson){
-            console.log(myJson);
-        });
+    const pickRandomVerb = (list) => {
+        var obj_keys = Object.keys(list);
+        var ran_key = obj_keys[Math.floor(Math.random() * obj_keys.length)];
+        return list[ran_key];
     }
 
-
-    const [data, setData] = React.useState();
-    const [verbDisplay, setVerbDisplay] = React.useState('');
-    const [correctConjugation, setCorrectConjugation] = React.useState('');
-    const [userAnswer, setUserAnswer] = React.useState('');
-    const [errorMode, setErrorMode] = React.useState(false);
-    const [correctFlag, setCorrectFlag] = React.useState(false);
-
-    const handleChange = (event) => {
-        setUserAnswer(event.target.value);
-    };
-
-    const checkSubmission = () => {
-
-        if (userAnswer === correctConjugation) {
-            setCorrectFlag(true);
-        } else {
-            setErrorMode(true);
-        }
-    };
+    const loadData = async() => {
+        const response = await fetch('verbs.json');
+        const json = await response.json();
+        setData(json);
+        console.log(json); //json is exactly what I want here
+        console.log(data); //data is just '[]' here
+    }
 
     useEffect(() => {
-        setVerbDisplay('Boire')
-        setErrorMode(false);
-        setCorrectFlag(true);
-    }, []
-    );
+
+        loadData();
+        console.log(data)
+
+        let source = pickRandomVerb(data)
+        console.log(source)
+
+        let verbSource = source.infinitive;
+
+        let showVerb = verbSource.toString().replaceAll("\"", "");
+
+        setDisplayVerb(showVerb)
+    
+    }, [])
+
 
     return(
         <div>
             <Typography className = {classes.form}>
-                <h3>proxy</h3>
+                <p>proxy {displayVerb}</p>
             </Typography>
-            <form className = {classes.form} noValidate autoComplete="off">
-                <FormControl error = {errorMode} variant = "outlined" margin="normal">
-                    <InputLabel htmlFor = "component-outlined">Article</InputLabel>
-                    <OutlinedInput id = "component-outlined" value = {userAnswer} onChange = {handleChange} label="Article"/>
-                    <FormHelperText id = "component-helper-text"></FormHelperText>
-                </FormControl>
-            </form>
-            <Button
-                variant="contained"
-                color="secondary"
-                className={classes.button}
-                startIcon={<CheckCircleOutlineIcon/>}
-                onClick = {checkSubmission}
-            >
-                Check
-            </Button>
         </div>
     )
 }
